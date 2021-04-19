0a59f51abc95d0bcc33c85e7923df748184ed42d
ed1868 <ruizeduardo21@gmail.com>
2021-04-19 13:51:52 -0400

    doer vids done and added array to loop over

---
+++ index.js
@@ -0,0 +1,20 @@
+let doers = [
+    {
+        name: "Eddie Ruiz",
+        position: "Software Engineer",
+        url: "https://www.facebook.com"
+
+    },
+    {
+        name: "Ronald Martinez",
+        position: "Chief Technology Officer",
+        url: "https://www.facebook.com"
+
+    },
+    {
+        name: "Victoria Brambilla",
+        position: "Software Engineer",
+        url: "https://www.facebook.com"
+
+    }
+];
